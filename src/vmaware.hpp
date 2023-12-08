1eb4956630693132b3cd2b289feb5c1b249c9dc6
Existential-Kernel <wpiffting8@gmail.com>
2023-12-08 17:12:31 +0000

    nothing works out in life at the end

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -510,6 +510,212 @@ private:
         #endif
     }
 
+    #if (MSVC)
+        /**
+         * @link: https://codereview.stackexchange.com/questions/249034/systeminfo-a-c-class-to-retrieve-system-management-data-from-the-bios
+         * @author: arcomber
+         */ 
+        class Systeminfo {
+        private:
+            #pragma pack(push) 
+            #pragma pack(1)
+            /*
+            SMBIOS Structure header (System Management BIOS) spec:
+            https ://www.dmtf.org/sites/default/files/standards/documents/DSP0134_3.3.0.pdf
+            */
+            struct SMBIOSHEADER
+            {
+                uint8_t type;
+                uint8_t length;
+                uint16_t handle;
+            };
+
+            /*
+            Structure needed to get the SMBIOS table using GetSystemFirmwareTable API.
+            see https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemfirmwaretable
+            */
+            struct SMBIOSData {
+                uint8_t  Used20CallingMethod;
+                uint8_t  SMBIOSMajorVersion;
+                uint8_t  SMBIOSMinorVersion;
+                uint8_t  DmiRevision;
+                uint32_t  Length;
+                uint8_t  SMBIOSTableData[1];
+            };
+
+            // System Information (Type 1)
+            struct SYSTEMINFORMATION {
+                SMBIOSHEADER Header;
+                uint8_t Manufacturer;
+                uint8_t ProductName;
+                uint8_t Version;
+                uint8_t SerialNumber;
+                uint8_t UUID[16];
+                uint8_t WakeUpType;  // Identifies the event that caused the system to power up
+                uint8_t SKUNumber;   // identifies a particular computer configuration for sale
+                uint8_t Family;
+            };
+            #pragma pack(pop) 
+
+            // helper to retrieve string at string offset. Optional null string description can be set.
+            const char* get_string_by_index(const char* str, int index, const char* null_string_text = "")
+            {
+                if (0 == index || 0 == *str) {
+                    return null_string_text;
+                }
+
+                while (--index) {
+                    str += strlen(str) + 1;
+                }
+                return str;
+            }
+
+            // retrieve the BIOS data block from the system
+            SMBIOSData* get_bios_data() {
+                SMBIOSData *bios_data = nullptr;
+
+                // GetSystemFirmwareTable with arg RSMB retrieves raw SMBIOS firmware table
+                // return value is either size of BIOS table or zero if function fails
+                DWORD bios_size = GetSystemFirmwareTable('RSMB', 0, NULL, 0);
+
+                if (bios_size > 0) {
+                    bios_data = (SMBIOSData*)malloc(bios_size);
+
+                    // Retrieve the SMBIOS table
+                    DWORD bytes_retrieved = GetSystemFirmwareTable('RSMB', 0, bios_data, bios_size);
+
+                    if (bytes_retrieved != bios_size) {
+                        free(bios_data);
+                        bios_data = nullptr;
+                    }
+                }
+
+                return bios_data;
+            }
+
+
+            // locates system information memory block in BIOS table
+            SYSTEMINFORMATION* find_system_information(SMBIOSData* bios_data) {
+
+                uint8_t* data = bios_data->SMBIOSTableData;
+
+                while (data < bios_data->SMBIOSTableData + bios_data->Length)
+                {
+                    uint8_t *next;
+                    SMBIOSHEADER *header = (SMBIOSHEADER*)data;
+
+                    if (header->length < 4)
+                        break;
+
+                    //Search for System Information structure with type 0x01 (see para 7.2)
+                    if (header->type == 0x01 && header->length >= 0x19)
+                    {
+                        return (SYSTEMINFORMATION*)header;
+                    }
+
+                    //skip over formatted area
+                    next = data + header->length;
+
+                    //skip over unformatted area of the structure (marker is 0000h)
+                    while (next < bios_data->SMBIOSTableData + bios_data->Length && (next[0] != 0 || next[1] != 0)) {
+                        next++;
+                    }
+                    next += 2;
+
+                    data = next;
+                }
+                return nullptr;
+            }
+
+        public:
+            // System information data retrieved on construction and string members populated
+            Systeminfo() {
+                SMBIOSData* bios_data = get_bios_data();
+
+                if (bios_data) {
+                    SYSTEMINFORMATION* sysinfo = find_system_information(bios_data);
+                    if (sysinfo) {
+                        const char* str = (const char*)sysinfo + sysinfo->Header.length;
+
+                        manufacturer_ = get_string_by_index(str, sysinfo->Manufacturer);
+                        productname_ = get_string_by_index(str, sysinfo->ProductName);
+                        serialnumber_ = get_string_by_index(str, sysinfo->SerialNumber);
+                        version_ = get_string_by_index(str, sysinfo->Version);
+
+                        // for v2.1 and later
+                        if (sysinfo->Header.length > 0x08)
+                        {
+                            static const int max_uuid_size{ 50 };
+                            char uuid[max_uuid_size] = {};
+                            _snprintf_s(uuid, max_uuid_size, max_uuid_size-1, "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+                                sysinfo->UUID[0], sysinfo->UUID[1], sysinfo->UUID[2], sysinfo->UUID[3],
+                                sysinfo->UUID[4], sysinfo->UUID[5], sysinfo->UUID[6], sysinfo->UUID[7],
+                                sysinfo->UUID[8], sysinfo->UUID[9], sysinfo->UUID[10], sysinfo->UUID[11],
+                                sysinfo->UUID[12], sysinfo->UUID[13], sysinfo->UUID[14], sysinfo->UUID[15]);
+
+                            uuid_ = uuid;
+                        }
+
+                        if (sysinfo->Header.length > 0x19)
+                        {
+                            // supported in v 2.4 spec
+                            sku_ = get_string_by_index(str, sysinfo->SKUNumber);
+                            family_ = get_string_by_index(str, sysinfo->Family);
+                        }
+                    }
+                    free(bios_data);
+                }
+            }
+
+            // get product family
+            const std::string get_family() const {
+                return family_;
+            }
+
+            // get manufacturer - generally motherboard or system assembler name
+            const std::string get_manufacturer() const {
+                return manufacturer_;
+            }
+
+            // get product name
+            const std::string get_productname() const {
+                return productname_;
+            }
+
+            // get BIOS serial number
+            const std::string get_serialnumber() const {
+                return serialnumber_;
+            }
+
+            // get SKU / system configuration
+            const std::string get_sku() const {
+                return sku_;
+            }
+
+            // get a universally unique identifier for system
+            const std::string get_uuid() const {
+                return uuid_;
+            }
+
+            // get version of system information
+            const std::string get_version() const {
+                return version_;
+            }
+
+            Systeminfo(Systeminfo const&) = delete;
+            Systeminfo& operator=(Systeminfo const&) = delete;
+
+        private:
+            std::string family_;
+            std::string manufacturer_;
+            std::string productname_;
+            std::string serialnumber_;
+            std::string sku_;
+            std::string uuid_;
+            std::string version_;
+        };
+    #endif
+
     // memoize the value from VM::detect() in case it's ran again
     static std::map<bool, std::pair<bool, const char*>> memo;
 
@@ -587,6 +793,7 @@ public:
         WINDOWS_NUMBER = 1ULL << 40,
         VMID_0X4 = 1ULL << 41,
         VPC_BACKDOOR = 1ULL << 42,
+        PARALLELS_VM = 1ULL << 43,
 
         NO_MEMO = 1ULL << 63,
         
@@ -2875,7 +3082,7 @@ private:
             return false;
         }
 
-        #if (!MSVC)
+        #if (!MSVC || !x86)
             return false;
         #else
             bool is_vm = false;
@@ -2967,44 +3174,73 @@ private:
 
 
     [[nodiscard]] static bool parallels() {
-        BYTE is_vm = 0;
-        ULONG dwDataSize = 0L;
-        PSYSTEM_FIRMWARE_TABLE_INFORMATION pSIF = NULL;
-        /* query parallels additions device presence */
-        is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS1);
-
-        if (is_vm == false) {
-            is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS2);
+        if (disabled(PARALLELS_VM)) {
+            return false;
         }
 
-        if (is_vm == false) {
-            is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS3);
-        }
+        return false;
 
-        /* scan raw firmware for specific string patterns */
-        if (is_vm == false) {
-            pSIF = (PSYSTEM_FIRMWARE_TABLE_INFORMATION)GetFirmwareTable(&dwDataSize, FIRM, 0xC0000);
-            if (pSIF != NULL && dwDataSize > 0) {
-                is_vm = ScanDump((CHAR*)pSIF, dwDataSize, VENDOR_PARALLELS, _strlenA(VENDOR_PARALLELS));
-                RtlFreeHeap(RtlProcessHeap(), 0, pSIF);
+
+        Systeminfo info;
+
+        #if __VMAWARE_DEBUG__
+            std::cout << std::left << ::std::setw(14) << "Manufacturer: " << info.get_manufacturer() << '\n'
+        << std::left << ::std::setw(14) << "Product Name: " << info.get_productname() << '\n'
+        << std::left << ::std::setw(14) << "Serial No: " << info.get_serialnumber() << '\n'
+        << std::left << ::std::setw(14) << "UUID: " << info.get_uuid() << '\n'
+        << std::left << ::std::setw(14) << "Version: " << info.get_version() << std::endl;
+
+            if (!info.get_family().empty()) {
+                std::cout << std::left << ::std::setw(14) << "Product family: " << info.get_family() << std::endl;
             }
-        }
 
-        /* scan raw SMBIOS firmware table for specific string patterns */
-        if (is_vm == false) {
-            pSIF = (PSYSTEM_FIRMWARE_TABLE_INFORMATION)GetFirmwareTable(&dwDataSize, RSMB, 0);
-            if (pSIF != NULL && dwDataSize > 0) {
-                is_vm = ScanDump((CHAR*)pSIF, dwDataSize, SMB_PARALLELS, _strlenA(SMB_PARALLELS));
-                RtlFreeHeap(RtlProcessHeap(), 0, pSIF);
+            if (!info.get_sku().empty()) {
+                std::cout << std::left << ::std::setw(14) << "SKU/Configuration: " << info.get_sku() << std::endl;
             }
-        }
+        #endif
 
-        /* query Parallels on PCI bus devices */
-        if (is_vm == false) {
-            if ( vIsInList(VID_PRLS) != NULL ) is_vm = 1;
-        }
+        #if (!MSVC)
+            return false;
+        #else
+            BYTE is_vm = 0;
+            ULONG dwDataSize = 0L;
+            PSYSTEM_FIRMWARE_TABLE_INFORMATION pSIF = NULL;
+            /* query parallels additions device presence */
+            is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS1);
 
-        return is_vm;
+            if (is_vm == false) {
+                is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS2);
+            }
+
+            if (is_vm == false) {
+                is_vm = IsObjectExists(DEVICELINK, DEVICE_PARALLELS3);
+            }
+
+            /* scan raw firmware for specific string patterns */
+            if (is_vm == false) {
+                pSIF = (PSYSTEM_FIRMWARE_TABLE_INFORMATION)GetFirmwareTable(&dwDataSize, FIRM, 0xC0000);
+                if (pSIF != NULL && dwDataSize > 0) {
+                    is_vm = ScanDump((CHAR*)pSIF, dwDataSize, VENDOR_PARALLELS, _strlenA(VENDOR_PARALLELS));
+                    RtlFreeHeap(RtlProcessHeap(), 0, pSIF);
+                }
+            }
+
+            /* scan raw SMBIOS firmware table for specific string patterns */
+            if (is_vm == false) {
+                pSIF = (PSYSTEM_FIRMWARE_TABLE_INFORMATION)GetFirmwareTable(&dwDataSize, RSMB, 0);
+                if (pSIF != NULL && dwDataSize > 0) {
+                    is_vm = ScanDump((CHAR*)pSIF, dwDataSize, SMB_PARALLELS, _strlenA(SMB_PARALLELS));
+                    RtlFreeHeap(RtlProcessHeap(), 0, pSIF);
+                }
+            }
+
+            /* query Parallels on PCI bus devices */
+            if (is_vm == false) {
+                if ( vIsInList(VID_PRLS) != NULL ) is_vm = 1;
+            }
+
+            return is_vm;
+        #endif
     }
 
     // __LABEL  (ignore this, it's just a label so I can easily teleport to this line on my IDE with CTRL+F)
@@ -3310,7 +3546,7 @@ const std::map<VM::u64, VM::technique> VM::table = {
     { VM::WINDOWS_NUMBER, { 20, VM::windows_number }},
     { VM::VMID_0X4, { 90, VM::vmid_0x4 }},
     { VM::VPC_BACKDOOR, { 70, VM::vpc_backdoor }},
-    { VM::PARALLELS, { 50, VM::parallels }}
+    { VM::PARALLELS_VM, { 50, VM::parallels }}
 
     // { VM::, { ,  }}
     // ^ line template for personal use
