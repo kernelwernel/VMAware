bbe13bdbd9183e9cc612ce9968e789617b3a7b1a
kernel <77142078+kernelwernel@users.noreply.github.com>
2024-04-04 15:45:00 +0100

    error test 3

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -6163,10 +6163,11 @@ const std::map<VM::u8, VM::core::technique> VM::core::table = {
     { VM::VMWARE_SCSI, { 40, VM::vmware_scsi }},
     { VM::VMWARE_DMESG, { 65, VM::vmware_dmesg }},
     //{ VM::VMWARE_EMULATION, { 20, VM::vmware_emul }},
-    //{ VM::VMWARE_STR, { 35, VM::vmware_str }},
-    //{ VM::VMWARE_BACKDOOR, { 100, VM::vmware_backdoor }},
-    //{ VM::VMWARE_PORT_MEM, { 85, VM::vmware_port_memory }},
+    { VM::VMWARE_STR, { 35, VM::vmware_str }},
+    { VM::VMWARE_BACKDOOR, { 100, VM::vmware_backdoor }},
+    { VM::VMWARE_PORT_MEM, { 85, VM::vmware_port_memory }},
     //{ VM::SMSW, { 30, VM::smsw }}
+    { VM::MUTEX, { 85, VM::mutex }}
 
     // __TABLE_LABEL, add your technique above
     // { VM::FUNCTION, { POINTS, FUNCTION_POINTER }}
