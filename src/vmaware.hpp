ecd55a26736d377ac231203314f94f858695a38f
kernel <77142078+kernelwernel@users.noreply.github.com>
2024-06-30 21:56:56 +0100

    ignore this i just need to checkout a pr but i need to do a commit smh

--- src/vmaware.hpp
+++ src/vmaware.hpp
@@ -609,9 +609,10 @@ private:
         // check for POSSIBILITY of hyperthreading, I don't think there's a 
         // full-proof method to detect if you're actually hyperthreading imo.
         [[nodiscard]] static bool has_hyperthreading() {
-            u32 eax, ebx, ecx, edx;
+            u32 unused, ebx, edx;
 
-            cpuid(eax, ebx, ecx, edx, 1);
+            cpuid(unused, ebx, unused, edx, 1);
+            UNUSED(unused);
 
             bool htt_available = (edx & (1 << 28));
 
@@ -7768,76 +7769,6 @@ private: // START OF PRIVATE VM DETECTION TECHNIQUE DEFINITIONS
     }
 
 
-#if (MSVC)
-        /**
-         * Hook information stored by Cuckoo at FS:[TLS_HOOK_INFO].
-         */
-        struct hook_info {
-            unsigned int depth_count;
-            unsigned int hook_count;
-            unsigned int retaddr_esp;
-            unsigned int last_error;
-            unsigned int ret_last_error;
-            unsigned int eax;
-            unsigned int ecx;
-            unsigned int edx;
-            unsigned int ebx;
-            unsigned int esp;
-            unsigned int ebp;
-            unsigned int esi;
-            unsigned int edi;
-        };
-
-        /**
-         * Read the address of the hooks information in the TLS.
-         */
-        struct hook_info *read_hook_info() {
-            void *result = NULL;
-
-            __asm__ volatile ("mov %%fs:0x44,%%eax" : "=a" (result));
-
-            return result;
-        }
-#endif
-
-    /**
-     * @brief Check for Cuckoo hook 
-     * @category Windows
-     * @author a0rtega
-     * @link https://github.com/a0rtega/pafish/blob/master/pafish/rtt.c
-     * @note from pafish project
-     * @copyright GPL
-     */ 
-    [[nodiscard]] static bool cuckoo_hook() try {
-#if (!MSVC)
-        return false;
-#else
-        #define TLS_HOOK_INFO_RETADDR_SPACE 0x100
-
-
-        /**
-         * Cuckoo stores the return addresses in a extra space allocated in conjunction
-         * with the hook information function. The only way to check if the structure
-         * is valid is to calculate what is the minimum and maximum value for the
-         * return address value location.
-         */
-
-            struct hook_info *info = read_hook_info();
-
-            if (info == NULL) {
-                return false;
-            }
-
-            unsigned int minimum = ((unsigned int) info + sizeof(struct hook_info));
-            unsigned int maximum = minimum + TLS_HOOK_INFO_RETADDR_SPACE;
-
-            return (info != NULL) && (info->retaddr_esp >= minimum && info->retaddr_esp <= maximum) ?
-                true : false;
-#endif
-    } catch (...) {
-        debug("MOUSE_DEVICE: catched error, returned false");
-        return false;
-    }
     
 
 
