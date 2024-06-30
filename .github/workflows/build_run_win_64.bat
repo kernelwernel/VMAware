b724c78c1eea87203dd1bee6dc6faeb57aeacb75
kernel <77142078+kernelwernel@users.noreply.github.com>
2024-06-30 18:22:50 +0100

    CI/CD artifact upload

--- .github/workflows/build_run_win_64.bat
+++ .github/workflows/build_run_win_64.bat
@@ -3,7 +3,6 @@ cd "%~dp0..\.."
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 17 2022" -A x64 -S ..
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "VMAware.sln" /Build "Release|x64" /Project "vmaware" /ProjectConfig "Release|x64"
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "VMAware.sln" /Build "Debug|x64" /Project "vmaware" /ProjectConfig "Debug|x64"
 cd Release
 vmaware.exe
-vmaware.exe --disable-hyperv-host
---
+++ .github/workflows/build_run_win_64_release.bat
@@ -0,0 +1,6 @@
+echo on
+cd "%~dp0..\.."
+mkdir build
+cd build
+cmake -G "Visual Studio 17 2022" -A x64 -S ..
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "VMAware.sln" /Build "Release|x64" /Project "vmaware" /ProjectConfig "Release|x64"
