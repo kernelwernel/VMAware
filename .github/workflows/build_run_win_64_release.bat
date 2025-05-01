36d62232b639d86a20d917a3883c2af733fb0f40
kernel <wpiffting8@gmail.com>
2025-05-01 17:41:50 +0100

    fixed windows release workflows

--- .github/workflows/build_run_win_64_release.bat
+++ .github/workflows/build_run_win_64_release.bat
@@ -8,5 +8,5 @@ mkdir build
 cd build
 cmake -G "Visual Studio 17 2022" -A x64 -S ..
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "VMAware.sln" /Build "Release|x64" /Project "vmaware" /ProjectConfig "Release|x64"
-cd Debug
+cd Release
 vmaware.exe
