4efd0eb94ec36a24d44c555548b8de847fab2307
kernelwernel <wpiffting8@gmail.com>
2025-10-17 14:34:08 +0100

    reformatted version message

--- src/cli.cpp
+++ src/cli.cpp
@@ -183,13 +183,13 @@ Extra:
 
 [[noreturn]] static void version(void) {
     std::cout << "vmaware " << "v" << ver << " (" << date << ")\n\n" <<
-    "Derived project of VMAware library at https://github.com/kernelwernel/VMAware"
+    "Derived project of VMAware library at https://github.com/kernelwernel/VMAware\n"
     "License MIT:<https://opensource.org/license/mit>.\n" << 
     "This is free software: you are free to change and redistribute it.\n" <<
     "There is NO WARRANTY, to the extent permitted by law.\n" <<
 
     "Developed and maintained by kernelwernel and Requiem,\n" << 
-    "see https://github.com/kernelwernel and https://github.com/NotRequiem\n";
+    "see https://github.com/kernelwernel and https://github.com/NotRequiem\n" <<
     "For any inquiries, contact us on Discord at shenzken or kr.nl, or email us at jeanruyv@gmail.com\n";
     std::exit(0);
 }
