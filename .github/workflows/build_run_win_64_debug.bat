1d69a9d224314c8f0a16da3cb187c8f47dd63edf
kernelwernel <wpiffting8@gmail.com>
2026-02-28 09:46:55 +0000

    fix: debug ci/cd fix attempt 1

--- .github/workflows/build_run_win_64_debug.bat
+++ .github/workflows/build_run_win_64_debug.bat
@@ -4,7 +4,7 @@ echo on
 cd "%~dp0..\.."
 mkdir build
 cd build
-cmake -DCMAKE_CXX_FLAGS="-D__VMAWARE_DEBUG__" .. -G "Visual Studio 17 2022" -A x64 -S ..
+cmake -DDEBUG_OUTPUT=ON .. -G "Visual Studio 17 2022" -A x64 -S ..
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.com" "VMAware.sln" /Build "Release|x64" /Project "vmaware" /ProjectConfig "Release|x64"
 cd Release
 vmaware.exe
